379fdd017317cb7b7826e02fa83c61b8aa9bab23
JavierFeria <macrotux@gmail.com>
2016-04-24 10:33:17 +0200

    Update SimpleMongoDb.php

--- src/SimpleMongoDb.php
+++ src/SimpleMongoDb.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace AppBundle\Controller;
+namespace \SimpleMongoDb;
 /**
  * Description of SimpleMongoDb
  *
